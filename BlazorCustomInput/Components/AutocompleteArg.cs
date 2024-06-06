fdec17633b444bd8603b1bcf7939f22f6dd5a73b
hayato takahashi <umai-beer@hotmail.co.jp>
2024-06-06 17:18:10 +0900

    ok

--- BlazorCustomInput/Components/AutocompleteArg.cs
+++ BlazorCustomInput/Components/AutocompleteArg.cs
@@ -9,10 +9,46 @@ namespace BlazorCustomInput.Components
 {
     public sealed class AutocompleteArg<Tval>
     {
-        public Tval? Value { get; init; }
-        public bool IsLoading { get; init; }
+        /// <summary>
+        /// 候補に上がった
+        /// </summary>
+        public Tval? Value { get; }
+        /// <summary>
+        /// 読み込み中
+        /// </summary>
+        public bool IsLoading { get; }
+        /// <summary>
+        /// 親の/でコンテンツに設定する選択イベント
+        /// </summary>
+        public EventCallback Selected { get; }
+        /// <summary>
+        /// コントロールに通知するコールバック
+        /// </summary>
+        public EventCallback<Tval> SelectCallback { get; }
 
-        public EventCallback<Tval> SelectCallback { get; init; }
-        
+        public AutocompleteArg()
+        {
+            IsLoading= true;
+            Value= default;
+            SelectCallback = EventCallback<Tval>.Empty;
+            Selected = EventCallback.Factory.Create(this, OnSelected);
+        }
+
+        public AutocompleteArg(Tval? value, bool isLoading, EventCallback<Tval> selectCallback):this()
+        {
+            Value = value;
+            IsLoading = isLoading;
+            SelectCallback = selectCallback;
+        }
+        /// <summary>
+        /// 選択されたことをコントロールに通知
+        /// </summary>
+        void OnSelected()
+        {
+            if(!IsLoading && SelectCallback.HasDelegate)
+            {
+                SelectCallback.InvokeAsync(Value);
+            }
+        }
     }
 }
