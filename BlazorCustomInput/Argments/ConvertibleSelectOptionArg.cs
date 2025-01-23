e9ee2addf29e4fbb5ca072248ef5155b95acf08e
hayato <umai-beer@hotmail.co.jp>
2025-01-23 17:33:01 +0900

    ok

---
+++ BlazorCustomInput/Argments/ConvertibleSelectOptionArg.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorCustomInput.Argments
+{
+    /// <summary>
+    /// コンバートタイプ選択ボックス
+    /// </summary>
+    /// <typeparam name="TSource"></typeparam>
+    /// <typeparam name="TVale"></typeparam>
+    public sealed class ConvertibleSelectOptionArg<TSource,TVale>
+    {
+
+        public TSource? SourceValue { get; }
+        public TVale? Value { get; }
+
+        public ConvertibleSelectOptionArg(TSource? sourceValue, TVale? value)
+        {
+            SourceValue = sourceValue;
+            Value = value;
+        }
+    }
+}
---
+++ BlazorCustomInput/Components/EditerConvertibleSelect.cs
@@ -0,0 +1,159 @@
+﻿using BlazorCustomInput.Argments;
+using BlazorCustomInput.Base;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+
+namespace BlazorCustomInput.Components
+{
+    /// <summary>
+    /// The convertible select is custom selectbox suported.
+    /// it also get property value from class object, and set it value to component.
+    /// </summary>
+    /// <typeparam name="Tval"></typeparam>
+    /// <typeparam name="TSource"></typeparam>
+    public class EditerConvertibleSelect<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)] Tval, [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)] TSource> : EditBase<Tval>
+    {
+        private readonly bool _isMultipleSelect;
+        private IEnumerable<TSource> _source;
+
+        /// <summary>
+        /// Gets or sets the child content to be rendering inside the select element.
+        /// </summary>
+        [Parameter] public RenderFragment? ChildContent { get; set; }
+        /// <summary>
+        /// Gets source to a select option. 
+        /// </summary>
+        [Parameter, EditorRequired]
+        public Func<IEnumerable<TSource>> Source { get; set; } = default!;
+        /// <summary>
+        /// The delegate is TSource convert to TVal.
+        /// </summary>
+        [Parameter, EditorRequired]
+        public Func<TSource,Tval> Converter { get; set; } = default!;
+
+        /// <summary>
+        /// Argment null value contents.
+        /// </summary>
+        [Parameter]
+        public RenderFragment? NullOptionContents { get; set; }
+        /// <summary>
+        /// option content
+        /// </summary>
+        [Parameter, Required]
+        public RenderFragment OptionContent { get; set; } = default!;
+
+        public EditerConvertibleSelect()
+        {
+            _isMultipleSelect = typeof(Tval).IsArray;
+            _source=Enumerable.Empty<TSource>();
+        }
+
+        protected override void OnParametersSet()
+        {
+            _source = Source();
+        }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            int index = 0;
+            builder.OpenElement(index++, "select");
+            builder.AddMultipleAttributes(index++, AdditionalAttributes);
+            builder.AddAttribute(index++, "class", CssClass);
+            builder.AddAttribute(index++, "multiple", _isMultipleSelect);
+
+            if (_isMultipleSelect)
+            {
+                builder.AddAttribute(index++, "value", BindConverter.FormatValue(CurrentValue)?.ToString());
+                builder.AddAttribute(index++, "onchange", EventCallback.Factory.CreateBinder<string?[]?>(this, SetCurrentValueAsStringArray, default));
+                builder.SetUpdatesAttributeName("value");
+            }
+            else
+            {
+                index += 3;
+                builder.AddAttribute(index++, "value", CurrentValueAsString);
+                builder.AddAttribute(index++, "onchange", EventCallback.Factory.CreateBinder<string?>(this, __value => CurrentValueAsString = __value, default));
+                builder.SetUpdatesAttributeName("value");
+            }
+
+            builder.AddElementReferenceCapture(index++, __inputReference => Element = __inputReference);
+            builder.AddElementReferenceCapture(index++, __selectReference => Element = __selectReference);
+
+            foreach(var item in _source)
+            {
+                var value = Converter(item);
+                var arg = new OptionArgment<Tval>(value);
+                
+                if (item is null)
+                {
+                    builder.OpenElement(index++, "option");
+                    builder.AddContent(index++, NullOptionContents);
+                    builder.CloseElement();
+                }
+                else
+                {
+                    index += 3;
+                    builder.OpenComponent<CascadingValue<int>>(index++);
+                    builder.AddAttribute(index++, nameof(CascadingValue<int>.Value), item.GetHashCode());
+                    builder.AddAttribute(index++, nameof(CascadingValue<int>.Name), nameof(ConvertibleOption<Tval>.ValueId));
+                    builder.AddAttribute(index++, nameof(CascadingValue<int>.ChildContent), (RenderFragment)((builder2) =>
+                    {
+                        builder2.OpenComponent<CascadingValue<int>>(index++);
+                        builder2.AddAttribute(index++, nameof(CascadingValue<int>.Value), value);
+                        builder2.AddAttribute(index++, nameof(CascadingValue<int>.Name), nameof(ConvertibleOption<Tval>.Value));
+                        builder2.AddAttribute(index++, nameof(CascadingValue<int>.ChildContent), (RenderFragment)((builder3) =>
+                        {
+                            builder3.AddContent(index++, OptionContent);
+                        }));
+                    }));
+                }
+            }
+            builder.CloseElement();
+        }
+
+        protected override bool ConvetTo(string? value, out Tval? result)
+        {
+            if (value is null or "")
+            {
+                result = default;
+                return true;
+            }
+            else
+            {
+                var enumrator=_source.GetEnumerator();
+                while (enumrator.MoveNext())
+                {
+                    var current= enumrator.Current;
+                    if (current is null) continue;
+                    if (current.GetHashCode().ToString() == value)
+                    {
+                        result =Converter(current);
+                        return true;
+                    }
+                }
+                result= default;
+                return false;
+            }
+        }
+
+        protected override string? FormatValueAsString(Tval? value)=> value?.GetHashCode().ToString();
+
+        protected override bool TryParseValueFromString(string? value, [MaybeNullWhen(false)] out Tval result, [NotNullWhen(false)] out string? validationErrorMessage)
+        {
+            var resultVal = ConvetTo(value, out result);
+            validationErrorMessage = resultVal ? string.Empty : ParsingErrorMessage;
+            return resultVal;
+        }
+
+        private void SetCurrentValueAsStringArray(string?[]? value)
+        {
+            if (value is null || value.Length == 0) return;
+
+            CurrentValue = BindConverter.TryConvertTo<Tval>(_source.Where(t=>value.Contains(t!.GetHashCode().ToString())).ToArray(), CultureInfo.CurrentCulture, out var result)
+                ? result
+                : default;
+        }
+    }
+}
